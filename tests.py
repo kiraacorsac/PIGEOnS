5e3927c15fe021f4c8d18cf6c5615396a42dc1fb
kiraacorsac <wire150@gmail.com>
2024-09-20 20:21:30 +0200

    tests: add more tests

--- tests.py
+++ tests.py
@@ -185,7 +185,7 @@ class NoDefaultName(Test):
             self.setState(TestState.WARNING)
             self.setFailedInfo(
                 None,
-                f"The following datablocks - {', '.join([obj.name for obj in wrongly_named_objects])} - use default names. "
+                f"The following datablocks - {', '.join([obj.name for obj in wrongly_named_objects])} - use default names.\n\n"
                 f"these are not descriptive and may confuse others working on the same scene, "
                 f"or others using these objects in their scene. Use descriptive names instead."
             )
@@ -212,7 +212,7 @@ class MaterialsSet(Test):
             self.setState(TestState.ERROR)
             self.setFailedInfo(
                 None,
-                f"The following datablocks - {', '.join([obj.name for obj in objects_missing_materials])} - don't have any materials assigned. "
+                f"The following datablocks - {', '.join([obj.name for obj in objects_missing_materials])} - don't have any materials assigned.\n\n"
                 f"These objects will have default 'grey' material in the render. We would like something better :)"
             )
 
@@ -234,7 +234,7 @@ class NoEmptyMaterialSlotsSet(Test):
             self.setState(TestState.ERROR)
             self.setFailedInfo(
                 None,
-                f"The following datablocks - {', '.join([obj.name for obj in objects_missing_materials])} - have empty material slots. "
+                f"The following datablocks - {', '.join([obj.name for obj in objects_missing_materials])} - have empty material slots.\n\n"
                 f"Parts of the object that are assigned empty material slot will have default 'grey' material in the render. We would like something better :)"
             )
 
@@ -260,7 +260,7 @@ class NoTris(Test):
         if len(objects_with_triangles) > 0:
             self.setFailedInfo(
                 None,
-                f"The following meshes - {', '.join([obj.name for obj in objects_with_triangles])} - have triangles. "
+                f"The following meshes - {', '.join([obj.name for obj in objects_with_triangles])} - have triangles.\n\n"
                 f"Triangles are faces with exactly 3 vertices. Triangular faces make it hard to edit the mesh, "
                 f"may deform incorrectly during animation, may cause shading issues, and don't play well with subdivision modifier. "
                 f"Stay quad!"
@@ -290,7 +290,7 @@ class NoNgons(Test):
         if len(objects_with_ngons) > 0:
             self.setFailedInfo(
                 None,
-                f"The following meshes - {', '.join([obj.name for obj in objects_with_ngons])} - have N-gons. "
+                f"The following meshes - {', '.join([obj.name for obj in objects_with_ngons])} - have N-gons.\n\n"
                 f"N-gons (for N > 4 :)) are faces with more then 4 vertices. N-gon faces make it hard to edit the mesh, "
                 f"may deform incorrectly during animation, will cause shading issues unless co-planar, "
                 f"and don't play well with subdivision modifier. Stay quad!"
@@ -315,7 +315,7 @@ class NoCrazySubdivision(Test):
         if len(objects_with_high_subdivision) > 0:
             self.setFailedInfo(
                 None,
-                f"The following objects - {', '.join([obj.name for obj in objects_with_high_subdivision])} - have really high subdivision. "
+                f"The following objects - {', '.join([obj.name for obj in objects_with_high_subdivision])} - have really high subdivision.\n\n"
                 f"It is almost definitely unnecessary to have this high subdivision level. Are you trying to make something look smooth? "
                 f"Have you tried right click -> shade smooth? That might do what you are trying to do."
             )
@@ -338,147 +338,169 @@ class RenderSubdivisionNotLessThenViewport(Test):
         if len(objects_with_incorrect_subdivision) > 0:
             self.setFailedInfo(
                 None,
-                f"The following objects - {', '.join([obj.name for obj in objects_with_incorrect_subdivision])} - have viewport subdivision level higher then render subdivision level. "
+                f"The following objects - {', '.join([obj.name for obj in objects_with_incorrect_subdivision])} - have viewport subdivision level higher then render subdivision level.\n\n"
                 f"This does not make sense. It will take more resources during working in the viewport, and look lower quality in the render."
             )
             self.setState(TestState.ERROR)
 
 
+@register_test
+class NoMaterialsWithoutNodes(Test):
+    label = "Materials use nodes"
+    homeworks = get_all_student_work_batteries()
 
-# @register_test
-# class DiffuseImageMapIsRGB(Test):
-#     label = "Diffuse Image Map Is sRGB"
-#     failedMessage = "Those textures are not in sRGB space:"
-#     homeworks = [2,3,4]
-#     def is_applicable(self,context):
-#         return True
-
-#     def execute(self,context):
-#         try:         
-#             objects = [] 
-#             for material in bpy.data.materials:
-#                 if material.use_nodes:
-#                     for node in material.node_tree.nodes:
-#                         if node.type == 'TEX_IMAGE':
-#                             image = node.image
-#                             if image and node.label == "Diffuse":
-#                                 color_space = image.colorspace_settings.name
-#                                 if color_space != 'sRGB':
-#                                     objects.append(material.name)
-
-#             if(objects == []):
-#                 self.setState(TestState.OK)
-#             else:
-#                 for obj in objects:
-#                     self.addObjectNameToMessage(obj.name)
-#                 self.setState(TestState.ERROR)
-
-#         except:
-#             self.traceback = traceback.format_exc()
-#             self.setState(TestState.CRASH)
-
-# @register_test
-# class NoInappropriateMetallness(Test):
-#     label = "No Inappropriate Metallness"
-#     failedMessage = "Materials should have 0.0 or 1.0 metallic values. \n Those materials:"
-#     homeworks = [2,3,4]
-#     def is_applicable(self,context):
-#         return True
-
-#     def execute(self,context):
-#         try:         
-#             objects = [] 
-#             for material in bpy.data.materials:
-#                 if material.use_nodes:
-#                     for node in material.node_tree.nodes:
-#                         if node.type == 'BSDF_PRINCIPLED':
-#                             metallic_value = node.inputs['Metallic'].default_value
-#                             if(metallic_value>0.01 and metallic_value<0.99):
-#                                 objects.append(material)
-
-#             if(objects == []):
-#                 self.setState(TestState.OK)
-#             else:
-#                 for obj in objects:
-#                     self.addObjectNameToMessage(obj.name)
-#                 self.setState(TestState.WARNING)
-
-#         except:
-#             self.traceback = traceback.format_exc()
-#             self.setState(TestState.CRASH)
+    def execute(self, context):
+        self.setState(TestState.OK)
+        nodeless_materials = []
+        for mat in utils.filter_used_datablocks(bpy.data.materials):
+            if not mat.use_nodes:
+                if mat.name == "Dots Stroke": # ignore weird inbuild material
+                    continue
+                nodeless_materials.append(mat)
+        if len(nodeless_materials) > 0:
+            self.setFailedInfo(
+                None,
+                f"The following materials - {', '.join([obj.name for obj in nodeless_materials])} - don't use nodes. Such materials are basically deprecated Blender.\n\n"
+                f"Please use only materials with nodes."
+            )
+            self.setState(TestState.ERROR)
 
 
-# @register_test
-# class NoFlatShading(Test):
-#     label = "No Flat Shading"
-#     failedMessage = "Don't use flat shading.\n Those objects use flat shading:"
-#     homeworks = [2,3,4]
-#     def is_applicable(self,context):
-#         return True
+@register_test
+class NoUnreallisticMetallness(Test):
+    label = "No unreallistic metallness"
+    homeworks = get_all_student_work_batteries()
 
-#     def execute(self,context):
-#         try:         
-#             objects = [] 
-#             for obj in bpy.context.scene.objects:
-#                 if obj.type == 'MESH':
-#                     flat_shaded = all(not poly.use_smooth for poly in obj.data.polygons)
-#                     if flat_shaded:
-#                         objects.append(obj)
+    def execute(self,context):
+        self.setState(TestState.OK)
+        hausler_alloys = [] 
+        for material in utils.filter_used_datablocks(bpy.data.materials):
+            if material.use_nodes:
+                for node in material.node_tree.nodes:
+                    if node.type == 'BSDF_PRINCIPLED':
+                        metallic_value = node.inputs['Metallic'].default_value
+                        if not (metallic_value < 0.01 or metallic_value > 0.99):
+                            hausler_alloys.append(material)
+                            
 
-#             if(objects == []):
-#                 self.setState(TestState.OK)
-#             else:
-#                 for obj in objects:
-#                     self.addObjectNameToMessage(obj.name)
-#                 self.setState(TestState.ERROR)
-
-#         except:
-#             self.traceback = traceback.format_exc()
-#             self.setState(TestState.CRASH)
-
-# @register_test
-# class NoSingleObject(Test): #object or mesh???
-#     label = "No Single Object"
-#     failedMessage = "There should not be single object.\n"
-#     homeworks = [2,3,4]
-#     def is_applicable(self,context):
-#         return True
-
-#     def execute(self,context):
-#         try:         
-#             objects = [] 
-#             nObjects = len(bpy.context.scene.objects)
-
-#             if(nObjects > 1):
-#                 self.setState(TestState.OK)
-#             else:
-#                 #for obj in objects:
-#                 #    self.addObjectNameToMessage(obj.name)
-#                 self.setState(TestState.ERROR)
-
-#         except:
-#             self.traceback = traceback.format_exc()
-#             self.setState(TestState.CRASH)
+        if len(hausler_alloys) > 0:
+            self.setFailedInfo(
+                None,
+                f"The following materials - {', '.join([obj.name for obj in hausler_alloys])} - use metalness values between 0 and 1.\n\n"
+                f"Such materials are not based on realistic properties - there are not many 'half-metals' in this world. "
+                f"Please, set up reallistic materials - either metals or non-metals."
+            )
+            self.setState(TestState.ERROR)
 
 
-# @register_test
-# class ReferencesPresentTest(Test): #object or mesh???
-#     label = "References Present"
-#     failedMessage = "There should be atleast two reference images.\n"
-#     homeworks = [2,3,4]
-#     def is_applicable(self,context):
-#         return True
+@register_test
+class NoFlatShading(Test):
+    label = "No Flat Shading"
+    homeworks = get_all_student_work_batteries() # TODO: just the chair homework
 
-#     def execute(self,context):
-#         try:         
-#             objects = [] 
-#             nObjects = len(bpy.context.scene.objects)
+    def execute(self,context):
+        self.setState(TestState.OK)
+        flat_shaded_objects = [] 
+        for obj in utils.filter_used_datablocks(bpy.data.objects):
+            if obj.type == 'MESH':
+                flat_shaded = all(not poly.use_smooth for poly in obj.data.polygons)
+                if flat_shaded:
+                    flat_shaded_objects.append(obj)
 
-#             if(nObjects > 1):
-#                 self.setState(TestState.OK)
-#             else:
-#                 self.setState(TestState.ERROR)
+        if len(flat_shaded_objects) > 0:
+            self.setFailedInfo(
+                None,
+                f"The following objects - {', '.join([obj.name for obj in flat_shaded_objects])} - use flat shading on one or more faces.\n\n"
+                f"This style of modelling calls for using smooth shading only. Flat shading will be visible in render. "
+                f"If you are trying to use flat shading on purpose, you can probably achieve the same results using weighted normals modifier."
+            )
+            self.setState(TestState.WARNING)
 
-#         except:
-#             self.traceback = traceback.format_exc()
-#             self.setState(TestState.CRASH)
+@register_test
+class NoSingleMeshObject(Test):
+    label = "No Single Mesh Object"
+    homeworks = get_all_student_work_batteries() #TODO: just the chair homework
+
+    def execute(self,context):
+        self.setState(TestState.OK)
+        if len(utils.filter_used_datablocks(obj for obj in bpy.data.objects if obj.type == 'MESH')) <= 1:
+            self.setState(TestState.ERROR)
+            self.setFailedInfo(
+                None,
+                f"This style of modelling calls for assembling the mesh from smaller parts. "
+                f"Do not build you model as a singular mesh, instead, model each part as a separate object. "
+            )
+
+
+
+@register_test
+class ReferencesPresent(Test):
+    label = "References Present"
+    homeworks = get_all_student_work_batteries()
+
+    def execute(self,context):
+        self.setState(TestState.OK)
+        reference_images = utils.filter_used_datablocks(obj for obj in bpy.data.objects if 'reference' in obj.name.lower())
+
+        if len(reference_images) < 2:
+            self.setFailedInfo(
+                None,
+                f"You need to use at least two references in this scene. We recommend using front and side references. "
+                f"Mark your references by including the word 'reference' in the name of the object that houses the image. "
+            )
+            self.setState(TestState.ERROR)
+        
+@register_test
+class PackedImages(Test):
+    label = "Packed Images"
+    homeworks = get_all_student_work_batteries()
+
+    def execute(self, context):
+        self.setState(TestState.OK)
+        unpacked_images = utils.filter_used_datablocks(img for img in bpy.data.images if img.packed_file is None and img.name != "Render Result")
+        if len(unpacked_images) > 0:
+            self.setFailedInfo(
+                None,
+                f"The following images are not packed - {', '.join([obj.name for obj in unpacked_images])} - please pack your resources for easier review process.\n\n"
+                f"Go to File -> External Data -> Pack Resources (or Automatically Pack Resources)."
+            )
+            self.setState(TestState.ERROR)
+
+
+@register_test
+class UseCycles(Test):
+    label = "Use Cycles"
+    homeworks = get_all_student_work_batteries() # only the material one
+
+    def execute(self, context):
+        self.setState(TestState.OK)
+        if context.scene.render.engine != 'CYCLES':
+            self.setState(TestState.ERROR)
+            self.setFailedInfo(
+                None,
+                f"Use Cycles rendering engine. More complex materials don't work in {context.scene.render.engine} out-of-the-box."
+            )
+
+@register_test
+class UseGPU(Test):
+    label = "Use GPU"
+    homeworks = get_all_student_work_batteries()
+
+    def is_applicable(self, context):
+        return context.scene.render.engine == 'CYCLES' 
+
+    def execute(self, context):
+        self.setState(TestState.OK)
+        has_gpu = len(bpy.context.preferences.addons.data.addons['cycles'].preferences['devices']) > 1 # assuming that if a computer has more then 1 device, it's a GPU
+        render_using_gpu = context.scene.cycles.device != 'GPU'
+        set_compute_device = bpy.context.preferences.addons.data.addons['cycles'].preferences['compute_device_type']  
+        if has_gpu and not (render_using_gpu and set_compute_device):
+            self.setState(TestState.WARNING)
+            self.setFailedInfo(
+                None,
+                f"Use your GPU for rendering. It will be probably much faster.\n\n"
+                f"In render properties, make sure Device is set to GPU Compute. "
+                f"In Preferences -> System -> Cycles Render Devices, make sure you have appropriate device type selected (probably CUDA or OptiX). "
+                f"Ignore this warning if you empirically observed that your CPU is faster (e.g. you have no dedicated GPU), "
+                f"or if Blender tells you that you have no compatible GPUs found."
+            )
